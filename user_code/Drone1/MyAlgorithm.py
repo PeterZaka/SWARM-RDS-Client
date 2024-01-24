74e748748b7481dd97e591f5ed631e28c1187f76
PeterZaka <PeterZaka70@gmail.com>
2024-01-24 17:37:03 -0500

    Made MyAlgorithm.py a blank template

--- user_code/Drone1/MyAlgorithm.py
+++ user_code/Drone1/MyAlgorithm.py
@@ -124,20 +124,6 @@ class MyAlgorithm(Algorithm):
             # SWARM will ignore these values
             return None
 
-    class Node:
-
-        def __init__(self, pos, parent=None):
-            self.pos = tuple(pos)
-            self.parent = parent
-            self.g = 0 # cost of path from n to start
-            self.h = 0 # estimated cost of path from n to end
-            self.f = 0 # total cost = g + h
-
-        def __eq__(self, other):
-            return self.pos[0] == other.pos[0] and self.pos[1] == other.pos[1]
-        
-        def __hash__(self):
-            return hash(self.pos)
 
 
     def myalgo(self, start, end, graph):
@@ -155,170 +141,17 @@ class MyAlgorithm(Algorithm):
 
         Returns
         -------
-        list of (x, y) or False
+        list of (x, y)
             path from start to end if found
         """
 
-        cost_grid = self.create_cost_grid(graph, [10, 5, 2.5, 1])
-        points = self.calculate_path(start, end, graph, cost_grid)
-
-        if len(points) == 0:
-            return []
-
-        points = self.prune_path(points)
-        points = self.line_of_sight_path(points, cost_grid, 5)
+        points = [start, end]
 
         return points
 
-    def create_cost_grid(self, grid, costs):
-        new_grid = copy.deepcopy(grid)
-
-        check_range = len(costs)
-        costs.insert(0, -1)
-        for r in range(len(grid)):
-            for c in range(len(grid[0])):
-                if grid[r][c] == 1:
-                    if c == 0 or grid[r][c - 1] == 1:
-                        left_check = 0
-                    else:
-                        left_check = -check_range
-
-                    if c == len(grid[0]) - 1 or grid[r][c + 1] == 1:
-                        right_check = 0
-                    else:
-                        right_check = check_range
-
-                    if r == len(grid) - 1 or grid[r + 1][c] == 1:
-                        up_check = 0
-                    else:
-                        up_check = check_range
-
-                    if r == 0 or grid[r - 1][c] == 1:
-                        down_check = 0
-                    else:
-                        down_check = -check_range
-
-                    for dx in range(left_check, right_check + 1):
-                        for dy in range(down_check, up_check + 1):
-                            x, y = c + dx, r + dy
-                            if 0 <= x < len(grid[0]) and 0 <= y < len(grid) and grid[y][x] != 1:
-                                new_grid[y][x] = max(new_grid[y][x], costs[max(abs(dx), abs(dy))])
-
-        return new_grid
 
 
-    def calculate_path(self, start, end, graph, cost_grid=[]):
-
-        open_list = []
-        closed_list = set()
-
-        start_node = self.Node(start)
-        index = 0
-        open_list.append((start_node.f, index, start_node))
-        heapq.heapify(open_list)
-
-        while len(open_list) > 0:
-            start_node = heapq.heappop(open_list)[-1]
-            closed_list.add(start_node)
-
-            for dir in [(0, 1), (1, 0), (0, -1), (-1, 0), (1, 1), (1, -1), (-1, -1), (-1, 1)]:
-                x = dir[0]
-                y = dir[1]
-
-                node = self.Node((start_node.pos[0] + x, start_node.pos[1] + y), start_node)
-                if node.pos[0] < 0:
-                    continue
-                if node.pos[0] >= len(graph[0]):
-                    continue
-                if node.pos[1] < 0:
-                    continue
-                if node.pos[1] >= len(graph):
-                    continue
-
-                if node in closed_list:
-                    continue
-
-                if graph[node.pos[1]][node.pos[0]] == 1:
-                    continue
-
-                node.g = start_node.g + math.sqrt(math.pow(start_node.pos[0] - node.pos[0], 2) + math.pow(start_node.pos[1] - node.pos[1], 2))
-                dup = any(node == n[-1] and n[-1].g <= node.g for n in open_list)
-                if dup:
-                    continue
-
-                node.h = math.sqrt(math.pow(end[0] - node.pos[0], 2) + math.pow(end[1] - node.pos[1], 2))
-                node.f = node.g + node.h
-                if len(cost_grid) != 0:
-                    node.f += cost_grid[node.pos[1]][node.pos[0]]
-
-
-                if node.pos[0] == end[0] and node.pos[1] == end[1]:
-                    path = []
-                    while node is not None:
-                        path.append(node.pos)
-                        node = node.parent
-
-                    path = path[::-1]
-                    return path
-
-                index += 1
-                heapq.heappush(open_list, (node.f, index, node))
-
-        return []
-    
-    def prune_path(self, path):
-        new_path = [path[0]]
-        for i in range(1, len(path) - 1):
-            dx = path[i][0] - path[i+1][0]
-            dx2 = path[i-1][0] - path[i][0]
-
-            dy = path[i][1] - path[i+1][1]
-            dy2 = path[i-1][1] - path[i][1]
-
-            if dx == dx2 and dy == dy2:
-                continue
-            new_path.append(path[i])
-        new_path.append(path[-1])
-        return new_path
-    
-    def is_line_of_sight_blocked(self, start, end, grid, threshold):
-        x1, y1 = start
-        x2, y2 = end
-
-        dy = y2 - y1
-        dx = x2 - x1
-
-        length = math.sqrt(dy**2 + dx**2)
-        vx = dx / length
-        vy = dy / length
-
-        for t in range(1, math.floor(length)):
-            x = round(x1 + t * vx)
-            y = round(y1 + t * vy)
-
-            if grid[y, x] >= threshold:
-                return True
-        
-        return False
-    
-    def line_of_sight_path(self, path, grid, threshold):
-        new_path = []
-
-        i = 0
-        while i < len(path):
-            start = path[i]
-            new_path.append(start)
-            for j in range(len(path) - 1, i, -1):
-                end = path[j]
-                if not self.is_line_of_sight_blocked(start, end, grid, threshold):
-                    new_path.append(end)
-                    i = j
-                    break
-
-            i += 1
-
-        return new_path
-
+    # Simulation helper functions
 
     def array_to_trajectory(self, points) -> Trajectory:
         """
